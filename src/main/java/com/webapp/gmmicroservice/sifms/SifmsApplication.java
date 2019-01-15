a07aed66ab58ba982a48f9288aa2075558c5a312
misonsan <misonsan@libero.it>
2019-01-15 16:59:45 +0100

    commit iniziale

---
+++ src/main/java/com/webapp/gmmicroservice/sifms/SifmsApplication.java
@@ -0,0 +1,14 @@
+package com.webapp.gmmicroservice.sifms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifmsApplication.class, args);
+	}
+
+}
+
